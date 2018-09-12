ec2815a61b3abb4a053a6d400517937cfa1c931b
cmphayes <42660033+cmphayes@users.noreply.github.com>
2018-09-11 23:07:57 -0400

    Modals fixed

--- WebApplication3/Scripts/JavaScript.js
+++ WebApplication3/Scripts/JavaScript.js
@@ -1,46 +1,45 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function ()
+{
 
-    
+    //$("#btnClear").click(function () {
+    //    $("#num1, #num2, #num3, #num4, #num5").val("");
+    //    $("#output1, #output2, #output3, #output4, #output5").html("");
+    //});
+
+    //$("#btnClearPal").click(function () {
+    //    $("#PalindromeWord").val("");
+    //});
+
+    //$("#btnClearFact").click(function () {
+    //    $("#FactorialNumber").val("");
+    //});
+
+    //$("#btnClearBuzz").click(function () {
+    //    $("#BuzzNum").val("");
+    //});
+
+    //$("#btnClearFizz").click(function () {
+    //    $("#FizzNum").val("");
+    //});
 
     $("#btnClear").click(function () {
         $("#num1, #num2, #num3, #num4, #num5").val("");
-        $("#output1, #output2, #output3, #output4, #output5").html("");
-    });
-
-    $("#btnClearPal").click(function () {
-        $("#PalindromeWord").val("");
-        $("#palout").html("");
-    });
-
-    $("#btnClearFact").click(function () {
+        $("#output1, #output2, #output3, #output4, #output5").val("");
+        $("#btnShowMathCode").hide("");
+        $("#PalindromeWord").text("");
+        $("#palout").text("");
+        $("#btnShowPalCode").hide("");
         $("#FactorialNumber").val("");
-        $("#factout").html("");
+        $("#factout").val("");
+        $("#btnShowFactCode").hide("");
+        $("FizzNum").val("");
+        $("BuzzNum").val("");
+        $("FizzBuzzOut").text("");
+        $("#btnShowFizzBuzzCode").hide("");
+
     });
-
-    $(document).ready(function () {
-       $('[data-toggle="popover"]').popover();
-    });
-
-    //$("#code").hide();
-
-    //$("#jscode").click(function () {
-    //    $("#code").toggle();
-    // if ($(this).text().indexof(show) > 0
-    //$("#btnshowcode").html("hide code <i class= fa fa caret up")
-    // else(
-    //$("#btnshowcode").html("show code")
-    //))
-    //}
-
-
-    //
-            //revword = revword.split("").reverse.join()
-
-   
-
 
     $("#btnCalc").click(function () {
-
 
         var num1 = $("#num1").val();
         var num2 = $("#num2").val();
@@ -48,82 +47,147 @@
         var num4 = $("#num4").val();
         var num5 = $("#num5").val();
 
+        if (num1 === "" || num2 === "" || num3 === "" || num4 === "" || num5 === "")
+        {
+            swal("Please Enter a Number in All Boxes")
+        }
+        else
+        {
+            var sum = num1 + num2 + num3 + num4 + num5;
+            var product = num1 * num2 * num3 * num4 * num5;
+            var mean = sum / 5;
+            var min = Math.min(num1, num2, num3, num4, num5);
+            var max = Math.max(num1, num2, num3, num4, num5);
 
-        var sum = num1 + num2 + num3 + num4 + num5;
-        var product = num1 * num2 * num3 * num4 * num5;
-        var mean = sum / 5;
-        var min = Math.min(num1, num2, num3, num4, num5);
-        var max = Math.max(num1, num2, num3, num4, num5);
-
-        $("#output1").html("The <b>sum</b> of your numbers is <b>" + sum + "</b>");
-        $("#output2").html("The <b>product</b> of your numbers is <b>" + product + "</b>");
-        $("#output3").html("The <b>mean</b> of your numbers is <b>" + mean + "</b>");
-        $("#output4").html("The <b>minimum</b> number entered is <b>" + min + "</b>");
-        $("#output5").html("The <b>maximum</b> number entered is <b>" + max + "</b>");
+            $("#output1").text("The <b>sum</b> of your numbers is <b>" + sum + "</b>");
+            $("#output2").text("The <b>product</b> of your numbers is <b>" + product + "</b>");
+            $("#output3").text("The <b>mean</b> of your numbers is <b>" + mean + "</b>");
+            $("#output4").text("The <b>minimum</b> number entered is <b>" + min + "</b>");
+            $("#output5").text("The <b>maximum</b> number entered is <b>" + max + "</b>");
+        }
     });
 
     $("#btnPal").click(function ()
     {
         var word = $("#PalindromeWord").val();
         var revWord = "";
-        var count = word.length
-        for (count >=0; count--;)
+
+        if (word === "")
         {
-            revWord = revWord+word.charAt(count)
-        }
-        if (word.toUpperCase() === revWord.toUpperCase())
-        {
-            //$("#palout").text(word + " is a palindrome");
-            alert(word + " is a palindrome");
+            swal("Please Enter a Word");
         }
         else
         {
-            //$("#palout").text(word + " is not a palindrome");
-            alert(word +  " is not a palindrome");
+            for (var count = word.length - 1; count >= 0; count--)
+            {
+                revWord = revWord + word.charAt(count);
+            }
+            if (word.toUpperCase() === revWord.toUpperCase()) {
+                $("#palout").text("your word, " + word + " is a palindrome");
+                //swal(word + " is a palindrome");
+            }
+            else {
+                $("#palout").text("your word, " + word + " is not a palindrome");
+                //swal(word +  " is not a palindrome");
+            }            
         }
     });
 
     $("#btnFact").click(function ()
     {
         var Fact = $("#FactorialNumber").val();
-        factfinal = 1;
-            for (i = Fact; i >= 1; i--);
+        var factfinal = 1;
+
+
+        if (Fact < 2 || Fact === "")
+        {
+            swal("Please Enter a Number Greater than 1");
+        }
+        else
+        {
+            for (i = Fact; i > 1; i--)
             {
                 factfinal *= i;
             }
-            $("#factout").html("<b>" + factfinal + "</b>");
-
+            $("#factout").text("<b>" + factfinal + "</b>");
+        }
     });
 
+    $("#btnFB").click(function () {
+
+        var FizzNum = $("#FizzNum").val();
+        var BuzzNum = $("#BuzzNum").val();
+
+        if (FizzNum < 0 || FizzNum > 100 || BuzzNum < 0 || BuzzNum > 100)
+        {
+            swal("Please Enter 2 Numbers in the Number Boxes that are Greater than 0 and less than 100")
+        }
+        else
+        {
+            var outputArray = []
+            var loop = 1
+
+            for (var loop = 1; loop <= 100; loop++) {
+                if (loop % FizzNum === 0 && loop % BuzzNum === 0) {
+                    outputArray.push("FizzBuzz");
+                }
+                else
+                    if (loop % FizzNum === 0) {
+                        outputArray.push("Fizz");
+                    }
+                    else
+                        if (loop % BuzzNum === 0) {
+                            outputArray.push("Buzz")
+                        }
+                        else {
+                            outputArray.push(loop);
+                        }
+            }
+            $("#FizzBuzzOut").text(outputArray.join(", "));
+        }
+    });
+
+    $("#btnShowMathCode").click(function () {
+        $("#MathCode").toggle();
+        if ($(this).text().indexOf("ShowCode") >= 0) {
+            $("#btnShowMathCode").html("HideCode <i class='fa fa-caret-up' />");
+        }
+        else {
+            $("#btnShowMathCode").html("ShowCode <i class='fa fa-caret-down' />");
+        }
+    });
+
+    $("#btnShowPalCode").click(function () {
+        $("#PalCode").toggle();
+        if ($(this).text().indexOf("ShowCode") >= 0) {
+            $("#btnShowPalCode").html("HideCode <i class='fa fa-caret-up' />");
+        }
+        else {
+            $("#btnShowPalCode").html("ShowCode <i class='fa fa-caret-down' />");
+        }
+    });
+
+    $("#btnShowFactCode").click(function () {
+        $("#FactCode").toggle();
+        if ($(this).text().indexOf("ShowCode") >= 0) {
+            $("#btnShowFactCode").html("HideCode <i class='fa fa-caret-up' />");
+        }
+        else {
+            $("#btnShowFactCode").html("ShowCode <i class='fa fa-caret-down' />");
+        }
+    });
+
+    $("#btnShowFizzBuzzCode").click(function () {
+        $("#FizzBuzzCode").toggle();
+        if ($(this).text().indexOf("ShowCode") >= 0) {
+            $("#btnShowFizzBuzzCode").html("HideCode <i class='fa fa-caret-up' />");
+        }
+        else {
+            $("#btnShowFizzBuzzCode").html("ShowCode <i class='fa fa-caret-down' />");
+        }
+    });
     
-    //$("ShowPalCode").click(function () {
-    //        $("ToggleCode").toggle();
-    //});
-
-
-
-    //show code buttons
-
-
-    //$(#MathCode).ready(function () {
-    //    $("button").click(function () {
-    //        $("p").toggle();
-
-
-    //    });
-    //});
-
-
-
-
-
 });
 
-
-
-
-
-
-
-
-
+    
+  
